71cd36598bf85e8be70526d28c20b64241840e36
Marcus Chong <marcus.jn.chong@gmail.com>
2019-05-10 20:36:04 -0400

    kinda working

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
-#!python3
+#!/usr/bin/env python3
 
+import ev3dev.ev3 as ev3
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,10 +8,23 @@ app = Flask(__name__)
 def smacc():
     try:
         print("eggplant smacc time")
+        n = ev3.LargeMotor('outA')
+
+      #  n.run_timed(time_sp=200, speed_sp=1000, polarity='inversed')
+
+
+        n.run_timed(time_sp=1, speed_sp=1000, polarity='normal')
+        n.run_timed(time_sp=230, speed_sp=1000, polarity='normal')
+        n.wait_until_not_moving()
+
+        
+
+        n.run_timed(time_sp=1, speed_sp=1000, polarity='inversed')
+        n.run_timed(time_sp=260, speed_sp=1000, polarity='inversed')
+
         return ("ok")
-    except:
-        return("big uh oh")
+    except Exception as e:
+        return(e)
 
 if __name__ == "__main__":
-	app.run()
-    
+	app.run(debug=True, host= '0.0.0.0')
